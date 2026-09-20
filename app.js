7fd99fd364c07011030f51c2d87aeda3b51f24c4
dipu01-code <dipendrakumarsenapati@gmail.com>
2026-09-20 16:02:23 +0530

    GET /assignments

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ app.post('/assignments', async (req,res) => {
         })
     }
 })
+
+app.get('/assignments', async (req, res) => {
+    try{
+        let result = await pool.query(
+            `SELECT * FROM assignments
+            ORDER BY id DESC;`
+        )
+        res.status(200).json(result.rows)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({
+            errorMessage: 'Server Down Ha Sir.'
+        })
+    }
+})
 app.listen(PORT, () => {
     console.log('Welcome to the server Null Vector');
 })
